a5070cd9c6e6ffd04a1a65a90123fa647c33a477
tales00 <talesworks@gmail.com>
2020-06-03 18:11:40 +0800

    intro 區塊完成

--- _frontend/src/views/intro.vue
+++ _frontend/src/views/intro.vue
@@ -33,16 +33,11 @@ export default {
 <style lang="scss" scoped>
 @import '~@/scss/_modules/_variables';
 .intro {
-  --intro-max-height: calc( 100vh - 3rem);
-  --intro-size: calc(var(--intro-max-height) / 3);
-  // position: relative;
-  height: var(--intro-max-height);
-  max-height: var(--intro-max-height);
+  --intro-height: calc(100vh - 3rem);
+  --intro-width: calc(100vw - 3rem);
   margin: 1.5rem;
-
+  height: var(--intro-height);
   display: grid;
-  grid-template-rows: var(--intro-size) auto 1fr auto;
-  // justify-items: center;
 
   background-image: url('~@/assets/img/bg_check_note.jpg');
   background-size: cover;
@@ -55,7 +50,7 @@ export default {
     height: 100%;
     width: 100%;
     object-fit: cover;
-    object-position: top left;
+    object-position: top right;
   }
 }
 
@@ -67,24 +62,21 @@ export default {
   transform: scaleY(0.8) skewX(-16deg);
   h3 {
     font-style: italic;
-    font-size: 5vmin;
-    margin: 1.3rem auto;
+    font-size: calc( var(--intro-width) / 18);
   }
   h1 {
-    font-size: 6vmin;
-    margin: 1.5rem auto;
+    font-size: calc( var(--intro-width) / 16);
   }
 }
 
 .avatar {
-  place-self: stretch;
-  display: grid;
   img {
     place-self: center;
     border-radius: 50%;
     box-shadow: 0 0.5em 1em rgba(0,0,0,0.5);
-    height: 50vmin;
+    
     max-height: 80%;
+    max-width: 60%;
   }
 }
 
@@ -92,136 +84,106 @@ nav {
   display: grid;
   padding: 1rem;
   grid-auto-flow: column;
-  place-self: stretch;
   justify-content: space-between;
   align-items: center;
   .entry {
-    width: calc( ((100vw - 3rem) - 2rem) / 7 );
-    max-width: 10vmin;
+    width: calc( ((100vmin - 3rem) - 2rem) / 7 );
+    max-width: 2.5rem;
     img {
       display: block;
       margin: auto;
     }
   }
 }
-
-// @media (max-height: $max-mobile) and (min-aspect-ratio: 480/320) {
-@media (min-aspect-ratio: 480/320) {
+// 橫 1.5:1 以內或者直的
+@media (max-aspect-ratio: 480/320) {
   .intro {
-    --intro-size: calc((100vw - 3rem) / 3);
+    grid-template-rows: calc(var(--intro-height) / 3) auto 1fr auto;
+  }
+  .advocate {
+    h3 { margin: 0.3em auto; }
+    h1 { margin: 0.5em auto; }
+  }
+  .avatar { display: grid; }
+}
+// 橫 1.5:1 或者更寬
+@media (min-height: $size-sm) and (min-width: $size-md), (min-aspect-ratio: 480/320) {
+  .intro {
+    position: relative;
+    grid-template-columns: calc(var(--intro-width) / 3) 1rem 1fr;
     grid-template-rows: auto 1fr;
-    grid-template-columns: var(--intro-size) 1fr;
-    justify-items: unset;
   }
 
   .intro-img { grid-row: span 2; }
 
-  .avatar {
-    position: absolute;
-    top: calc( 50vh - 6rem);
-    left: calc(100vw / 3 - 3rem);
-    img{
-      height: 24vmin;
-      
+  .advocate {
+    grid-column: 3;
+    text-align: left;
+    h3 {
+      font-size: calc( var(--intro-width) / 28);
+      margin: 3vmin auto;
+      margin-left: 1rem;
+    }
+    h1 {
+      font-size: calc( var(--intro-width) / 22);
+      margin: 6vmin auto;
+      margin-left: 1rem;
     }
   }
 
-  .advocate {
-    text-align: left;
-    padding-left: 1rem;
-    h3 {
-      font-size: 4vmin;
-      margin: 2vmin;
-    }
-    h1 {
-      font-size: 6vmin;
-      margin: 4vmin 2vmin;
+  .avatar {
+    grid-column: 2;
+    grid-row: span 2;
+    img{
+      position: absolute;
+      top: 50%;
+      left: calc(var(--intro-width) / 3);
+      width: calc(var(--intro-height) / 3);
+      height: calc(var(--intro-height) / 3);
+
+      transform: translateX(-50%) translateY(-50%);;
     }
   }
 
   nav {
-    padding: 4vmin;
+    padding: 1rem;
+    padding-left: 0;
     align-self: end;
     .entry {
-      width: calc( ((var(--intro-size) * 2 - 2rem) / 7 ));
+      width: calc( ((var(--intro-width) / 3 * 2 ) - 2rem) / 7 );
     }
   }
 }
-@media (min-height: $min-tablet) and (max-aspect-ratio: 320/568) {
+@media (min-height: $size-sm) {
   .intro {
-    --intro-max-height: calc( var(--size-md) * 0.9);
-    --intro-size: calc(var(--intro-max-height) / 3);
-    
-  }
-}
-@media (min-height: $min-tablet) and (min-width: $min-tablet) {
-  .intro {
-    --intro-max-width: calc( var(--size-md) - 3rem);
-    --intro-max-height: calc( (var(--intro-max-width) * 1.6 ) - 6rem );
-    height: calc( 100vh - 6rem);
-    max-height: var(--intro-max-height);
-    margin: 3rem;
+    --intro-height: calc(var(--size-sm) - 3rem);
   }
   .advocate {
-    letter-spacing: 0.5rem;
-  }
-  @media (max-aspect-ratio: 480/320){
-    .intro {
-      --intro-size: calc( var(--intro-max-height) / 3);
-      max-width: var(--intro-max-width);
-      margin: 3rem auto;
-    }
-    .advocate {
-      h3 {
-        font-size: 1.2rem;
-        margin: 1rem;
-      }
-      h1 {
-        font-size: 1.6rem;
-        margin: 1rem;
-      }
-    }
-
-    .avatar {
-      img {
-        height: calc( var(--intro-max-width) * 24% );
-        max-height: 80%;
-      }
-    }
-
-    nav {
-      justify-content: space-evenly;
-      .entry {
-        width: calc( (var(--intro-max-width) - 2rem) / 7 );
-        max-width: 8vmin;
-      }
-    }
+    h3 { margin-top: 1rem; margin-bottom: 0;}
+    h1 { margin-top: 1.5rem; margin-bottom: 0;}
   }
 }
-
-@media (min-width: $min-wide) and (min-aspect-ratio: 480/320) {
+@media (min-height: $size-md) {
   .intro {
-    --intro-max-width: calc( var(--size-xl) - 6rem);
-    --intro-max-height: calc( (var(--intro-max-width) * 0.8 ) - 6rem );
-    --intro-size: calc(var(--intro-max-width) / 3);
+    --intro-height: calc(var(--size-md) - 3rem);
   }
-  .advocate {
-    h3 {
-      font-size: 1.5rem;
-      margin: 2rem;
-    }
-    h1 {
-      font-size: 2.5rem;
-      margin: 2rem;
-    }
+}
+@media (min-width: $size-md) {
+  .intro {
+    --intro-width: calc(var(--size-md) - 3rem);
+    grid-template-columns: 1fr 1rem auto;
   }
   .avatar {
-    top: calc( (var(--intro-max-height) / 2) + 3rem);
-    left: calc( ((100vw - var(--intro-max-width)) / 2) + var(--intro-size) );
-    transform: translate(-50%, -50%);
+    img{
+
+      left: unset;
+    }
   }
   nav {
-    padding: 2rem;
+    justify-content: flex-end;
+    .entry {
+      margin: 0 1rem;
+    }
   }
 }
 </style>
