679031a57d68fa4aaa4ee90126979431b31b256e
tales00 <talesworks@gmail.com>
2020-06-05 17:12:02 +0800

    開啟 contact modal

--- _frontend/src/views/modal/contact.vue
+++ _frontend/src/views/modal/contact.vue
@@ -1,6 +1,13 @@
 <template lang="pug">
   .modal-contact
-    | CONTACT
+    h4 mail : talesworks@gmail.com
+    h4 follow my activity or Contact me on :
+    a(href="https://plurk.com/taleslogs")
+      img.icon(src="@/assets/img/logo-plurk.png")
+      | plurk
+    a(href="https://pixiv.me/tales00") 
+      img.icon(src="@/assets/img/logo-pixiv.svg")
+      | pixiv
 </template>
 
 <script>
@@ -11,6 +18,17 @@ export default {
 
 <style lang="scss" scoped>
 .modal-contact {
+  a {
+    margin: 1rem auto;
+    display: grid;
+    grid-template-columns: 4rem auto;
+    grid-gap: 1rem;
 
+    align-items: center;
+
+    img {
+      max-width: 100%;
+    }
+  }
 }
 </style>
