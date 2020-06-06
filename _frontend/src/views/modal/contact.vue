b1f006423e817ae94e7a553f13a168cfc5b11e9a
tales00 <talesworks@gmail.com>
2020-06-06 16:48:13 +0800

    調整 contact 版面

--- _frontend/src/views/modal/contact.vue
+++ _frontend/src/views/modal/contact.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   .modal-contact
-    h4 mail : talesworks@gmail.com
     h4 follow my activity or Contact me on :
+    a(href="mailto://talesworks@gmail.com")
+      img.icon(src="@/assets/img/envelope.svg")
+      | talesworks@gmail.com
     a(href="https://plurk.com/taleslogs")
       img.icon(src="@/assets/img/logo-plurk.png")
       | plurk
@@ -18,10 +20,13 @@ export default {
 
 <style lang="scss" scoped>
 .modal-contact {
+  background-color: var(--secndary-light);
+  color: var(--secndary-text-dark);
+  padding: 2rem;
   a {
     margin: 1rem auto;
     display: grid;
-    grid-template-columns: 4rem auto;
+    grid-template-columns: 2.5rem auto;
     grid-gap: 1rem;
 
     align-items: center;
