5ba6fd6829a27d851f8220d47325c45967cd2d28
tales00 <talesworks@gmail.com>
2020-06-06 18:41:11 +0800

    setup header navi

--- _frontend/src/views/header-nav.vue
+++ _frontend/src/views/header-nav.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
-  nav.header nav nav
+  nav.navi
+    h2.sectionName sectionName
+    router-link.routeLink(:to="{name:'about'}") about
+    router-link.routeLink(:to="{name:'blog'}") blog
+    router-link.routeLink(:to="{name:'illust'}") illust
 </template>
 
 <script>
@@ -9,7 +13,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-nav.header {
-
+.navi {
+  padding: 0.4rem 1.5rem;
+  display: grid;
+  grid-auto-flow: column;
+  grid-template-columns: 1fr auto auto auto;
+  grid-gap: 1rem;
 }
 </style>
