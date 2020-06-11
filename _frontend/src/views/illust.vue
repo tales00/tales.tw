b5b8f553eee56781739a63e01cb97d8289944928
tales00 <talesworks@gmail.com>
2020-06-11 18:23:02 +0800

    新增顯示插圖的 modal

--- _frontend/src/views/illust.vue
+++ _frontend/src/views/illust.vue
@@ -1,15 +1,22 @@
 <template lang="pug">
-  .illust
-    masonry-block( :colCount="3" )
-      masonry-item(
-        v-for=" illust in illusts"
-      )
-        img(:src="`//images.weserv.nl/?we&w=360&url=pixiv.cat/${illust.id}${illust.pageCount>1?'-1':''}.jpg`")
+  .sect-illust
+    masonry-block( v-if="isPixivLoaded" )
+      masonry-item( v-for=" (illust, idx) in illusts" )
+        .illustCard(@click="showPixivIllustModal(illust.id)")
+          img.illust(
+            :alt="illust.alt"
+            :src="`//images.weserv.nl/?we&w=360&url=pixiv.cat/${illust.id}${illust.pageCount>1?'-1':''}.jpg`"
+            referrerpolicy="no-referrer"
+          )
+          time.illustCreateDate {{ illust.createDate.match(/\d{4}-\d{2}-\d{2}/)[0] }}
+          h3.illustTitle {{ illust.title }}
 </template>
 
 <script>
 import { createNamespacedHelpers } from 'vuex'
 import { masonryBlock, masonryItem } from '@/components/masonryBlock'
+import ShowPixivIllust from '@/views/modal/ShowPixivIllust'
+
 const { 
   mapState:mapPixivState,
   mapGetters:mapPixivGetters, 
@@ -25,34 +32,54 @@ export default {
       isPixivLoaded: 'isLoaded'
     }),
   },
+  // watch: { },
   methods: {
     ...mapPixivActions({
       fetchPixivUser: 'fetchUser'
     }),
+
+    showPixivIllustModal(illustId) {
+      
+      this.$dialog.show( {
+        component: ShowPixivIllust,
+        paramObj: {
+          dialog: {
+            props: {
+              illustId,
+              userId: '743865'
+            },
+            events: {
+              'onIllustLoaded': () => {
+                console.log('got loaded event');
+              }
+            }
+          },
+        }
+      } );
+    },
   },
   created() {
-    if( !this.isPixivLoaded ) {
+    if( 'illust' === this.$route.name && !this.isPixivLoaded ) {
       console.log('fetch pixiv');
-      
       this.fetchPixivUser(743865);
     } else {
       console.log('not fetch pixiv');
-      
     }
-  }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 @import '~@/scss/_modules/_variables';
 
-.illust {
+.sect-illust {
   margin: 1.5rem;
 }
 
 .masonryBlock {
   --col-count: 2;
-  --col-gap: 0.5rem;
+  --col-gap: 0.6rem;
+  --row-gap: 0;
   // --row-gap: 1rem;
   // @media (min-width: $size-xs) { --col-count: 2; }
   @media (min-width: $size-sm) { --col-count: 3; }
@@ -61,8 +88,24 @@ export default {
   // @media (min-width: $size-xl) { --col-count: 6; }
   // @media (min-width: $size-xxl) { --col-count: 7; }
 
-  .masonryItem {
-    img { width: 100%; }
+  .masonryItem { }
+}
+
+.illustCard {
+  // border: 1px solid #000;
+  margin-bottom: 1.2rem;
+  .illust {
+    width: 100%;
+    display: block;
+    border: 4px solid var(--gray-4);
+  }
+  .illustCreateDate {
+    font-size: 0.5rem;
+  }
+  .illustTitle {
+    font-size: 0.9rem;
+    font-weight: bold;
+    line-height: 125%;
   }
 }
 </style>
---
+++ _frontend/src/views/modal/ShowPixivIllust.vue
@@ -0,0 +1,125 @@
+<template lang="pug">
+  article.modal-showPixivIllust(v-if="isIllustLoaded")
+    picture
+      img(
+        v-for="idx in illust.pageCount"
+        :src="`//images.weserv.nl/?url=pixiv.cat/${illustId}${illust.pageCount>1?'-'+idx:''}.jpg`"
+        referrerpolicy="no-referrer"
+      )
+    .illustInfo
+      time.illustCreateDate {{ illust.createDate.match(/\d{4}-\d{2}-\d{2}/)[0] }}
+      h2.illustTitle
+        a(:href="`//www.pixiv.net/artworks/${illustId}`" target="_blank")
+          | {{ illust.title }}
+      .author
+        a.avatar(:href="user.URL" target="_blank")
+          img(:src="user.image" referrerpolicy="no-referrer")
+          span.authorName by {{ user.name }}
+      .tags
+        a.tag(
+          v-for=" (tag, idx) in illust.tags"
+          :href="`https://www.pixiv.net/users/${userId}/artworks/${ tag.tag }`"
+          target="_blank"
+        )
+          | {{ tag.tag }}
+</template>
+
+<script>
+import { getPixivIllustData } from '@/api/_api.tales.tw'
+
+export default {
+  name: 'modal-showPixivIllust',
+  props: {
+    illustId: { type: [String, Number] },
+    userId: { type: [String, Number], },
+  },
+  data() { return {
+    illust: {},
+    user: {},
+    isIllustLoaded: false,
+  }},
+  // computed: { },
+  methods: {
+    fetchPixivIllust( illustId, userId ) {
+      getPixivIllustData( illustId ).then( ({illust, user}) => {
+        if( userId && userId !== user.userId ) {
+          this.$emit('onUserError');
+        }
+        this.illust = illust;
+        this.user = user;
+        this.isIllustLoaded = true;
+
+        this.$emit('onIllustLoaded');
+      });
+
+    }
+  },
+  created() {
+    this.fetchPixivIllust( this.illustId, this.userId );
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-showPixivIllust {
+  color: var(--secndary-text-dark);
+  display: grid;
+  max-height: inherit;
+
+  @media (orientation: Portrait) {
+    overflow-y: auto;
+  }
+  @media (orientation: landscape) {
+    grid-template-columns: 1fr 30%;
+    
+    picture {
+      max-height: inherit;
+      overflow-y: auto;
+    }
+  }
+}
+picture {
+  background: var(--secndary-light);
+}
+.illustInfo {
+  padding: 0.5rem;
+  background-color: hsla(0, 0%, 100%, 0.7);
+
+  @media (orientation: Portrait) {
+    position: sticky;
+    bottom: 0;
+  }
+}
+.illustCreateDate {
+  font-size: 0.6rem;
+}
+.illustTitle {
+  font-weight: bold;
+}
+.author {
+  display: flex;
+  flex-flow: row-reverse;
+  margin: 1rem auto;
+}
+.avatar {
+  display: block;
+  img { margin: auto; }
+}
+.authorName {
+  font-size: 0.8rem;
+}
+.tags {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 1rem auto;
+}
+.tag {
+  font-size: 0.6rem;
+  line-height: 100%;
+  color: var(--gray-0);
+  background-color: var(--gray-3);
+  margin-bottom: 0.2rem;
+  margin-right: 0.6rem;
+  padding: 0.2rem 0.5rem 0.2rem 0.2rem;
+}
+</style>
