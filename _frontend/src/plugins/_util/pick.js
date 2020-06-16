44a89c86f0cd3de2c392c44cdb31f238ad1f443c
tales00 <talesworks@gmail.com>
2020-06-16 19:40:03 +0800

    修改 pick

--- _frontend/src/plugins/_util/pick.js
+++ _frontend/src/plugins/_util/pick.js
@@ -1,73 +1,45 @@
-// optional chaining
-export const pick = (input, chaining=null, cb, defaultVal) => [cb].reduce(
-  // target 就是最後要得到的物件，
-  // cb 是（如果 target 是函數的話）要傳入的參數
-  // 如果 target 是函數，並且 cb 是陣列而且有內容，就執行函數
-  // 如果 target 不是函數，又或者 cb 是空的，
-  // 再看 target 跟最初傳入的 input 是否"不同"
-  // 不同的話就回傳 target  或 defaultVal
-  // 相同的話就回傳 defaultVal 或是 false
-  target=>(target && typeof cb==='function')
-    ? cb(target)
-    : (target!==input)
-      ? (typeof target!=='undefined')
-        ? target
-        : defaultVal
-      : defaultVal
-  ,
-  // 傳給上面用的參數 target：
-  // 如果 chaining 沒東西或不是字串，就是 input 本身
-  // 如果 chaining 是字串而且有內容，就把 chaining 拆開，一個一個測試
-  // 測試到最後一個就是 target 
-  (!chaining || typeof chaining!=='string')
-    ? input 
-    : chaining
-      .replace(/(\])/g, '$1.').replace(/\.$/, '').split('.')
-      .reduce( (ac, key)=>/^(\w*)\[(['|"]?)(.+)(\2)\]$/.test(key)
-        ? key.match(/^(\w*)\[(['|"]?)(.+)(?:\2)\]$/).splice(1).filter(val=>!/^'|"$/.test(val))
-          .reduce((a_prop, c_prop)=>(!a_prop)
-            ? ac[c_prop]
-            : (typeof a_prop==='string')
-              ? (typeof ac[a_prop][c_prop]==='function')
-                ? ac[a_prop][c_prop]
-                : ac[a_prop]
-              : a_prop[c_prop]
-          )
-        : (typeof ac==='object' && key in ac)
-          ? ac[key]
-          : undefined
-      , input )
-);
+// 模仿 optional chaining
+// input: 要檢查的物件
+// chainString: 要檢查的路徑 'aaa.bbb['ccc'][0].ddd[1]' 像這樣的東西
+// fallback: 找不到的話要返回什麼
+// call: 如果找到的是函數的話要不要直接執行
+// props: 如果要執行函數的話要給的參數，必須是 Array
+const pick = (input, chaingString, {fallback, call, props=[]}={}) => {
+  // input 若不是物件 或 chaingString 不是字串 ，就 return input
+  if (
+    (typeof input !== 'object')
+    || (!chaingString || typeof chaingString !== 'string')
+  ) { return input; }
 
-/*
+  // 將 chaingString 拆成以 '.' 為分隔的字串，再轉成陣列
+  const $chainingArray = chaingString
+  .replace(/(\[(['"])?|(['"])?\])/g, '.') // 將 [' 或 '] 改成 . （包含單/雙引號有無）
+  .replace(/\.+/g, '.') // 將連續多個的 . 改成只有一個
+  .replace(/^\.+|\.+$/g, '') // 將頭尾的 . 刪掉
+  .split('.');
 
-pick( <查找的物件>[, <要查找的路徑>, [ <callback>, [ <沒找到時的預設值: undefind> ]]] )；
+  // 遞迴取出目標
+  const $result = $chainingArray.reduce((acc, key) => {
+    if (
+      (acc !== null || acc !== undefined)
+      && (typeof acc === 'object')
+    ) { return acc[key]; }
+  }, input);
 
-const someObj = {
-  some: {
-    prop: {
-      valA: 'this is val A',
-      valB: 'this is val B',
-      valC: greeting=>`hello ${greeting} !`
-    },
-    attr: [ '111', '222', '333',
-      { qqq: 'you find QQQ' }
-    ],
-    fn() { return 'fn no need params'; }
-  }
-};
+  // 回傳或者執行目標
+  return fallback && $result === undefined 
+    ? ( call && typeof fallback === 'function'? fallback(...props) : fallback )
+    : ( call && typeof $result === 'function'? $result(...props) : $result )
+}
 
-console.log(
-  pick(someObj, 'some.prop.valC', fn=>fn('world')), // 'hello world'
-  pick(someObj, 'some.prop["valC"]', fn=>fn('eirth')), // 'hello eirth'
-  pick(someObj.some.prop.valC, '', fn=>fn('space')), // 'hello space'
-  pick(someObj, 'some.attr[3]["qqq"]'), // 'you find QQQ'
-  pick(someObj.some.attr[3], '["qqq"]'), // 'you find QQQ'
-  pick(someObj.some.attr, '[0]'), // 111
-  pick(someObj, 'aaa.bbb.ccc', null, 'default value'), // 'default value'
-  pick(someObj, 'some.fn'), // 得到 fn 本身
-  pick(someObj, 'some.fn', fn=>{fn()}), // 執行 fn： 'fn no need params'
-  pick(someObj, 'some.fn2', fn=>{fn()}), // undefind
-  pick(someObj, 'some.fn2', fn=>{fn()}, 'can't find function'), // 'can't find function'
-)
-*/
+// const someObj = {
+//   prop: {
+//     valA: 'this is val A',
+//     valB: 'this is val B',
+//     valC: greeting=>`hello ${greeting} !`
+//   },
+//   attr: [ '111', '222', '333',
+//     { qqq: 'you find QQQ' }
+//   ],
+//   fn() { return 'fn no need params'; }
+// };
